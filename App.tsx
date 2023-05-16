60db84956f031ef725c39cfbc3ad2408d1c51e62
Mr Cthulhu <danielparucho@gmail.com>
2023-05-16 17:52:32 -0300

    Fix navegación

--- App.tsx
+++ App.tsx
@@ -1,52 +1,65 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from './views/Home';
-import Favorites from './views/Favorites';
+import Collection from './views/Collection';
+import PokemonSelection from './views/PokemonSelection';
 import About from './views/About';
 import Login from './views/Login';
 import Registry from './views/Registry';
+import Avatar from './views/AvatarSelection';
 import ContextProvider from './context';
 import { QueryClient, QueryClientProvider } from 'react-query';
-// Navegador, pantallas de rutas, estilos de iconos.
+
+const options = ({ route }: { route: any }) => ({
+  tabBarIcon: ({ color }: { color: string }) => {
+    let icon = "";
+    if (route.name === 'Pokedex') {
+      icon = 'crosshairs';
+    } else if (route.name === 'Colección') {
+      icon = 'pokeball';
+    } else if (route.name === 'Acerca') {
+      icon = 'information';
+    }
+    return <Icon name={icon} size={30} color={color} />;
+  },
+  tabBarActiveTintColor: '#E00004',
+  tabBarInactiveTintColor: 'gray',
+  headerShown: false,
+})
+
+function NavigationWithTabs() {
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <Tab.Navigator initialRouteName="Login"
+      screenOptions={options}
+    >
+      <Tab.Screen name="Pokedex" component={Home} />
+      <Tab.Screen name="Colección" component={Collection} />
+      <Tab.Screen name="Acerca" component={About} />
+    </Tab.Navigator>
+  );
+}
 
 function App(): JSX.Element {
-  const Tab = createBottomTabNavigator();
-  // usamos react-query.
+
   const queryClient = new QueryClient();
+  const Stack = createNativeStackNavigator();
+
   return (
     <QueryClientProvider client={queryClient}>
       <ContextProvider>
         <NavigationContainer>
-          <Tab.Navigator initialRouteName="Login"
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ color }) => {
-                let icon = "";
-                if (route.name === 'Home') {
-                  icon = 'home';
-                } else if (route.name === 'Favoritos') {
-                  icon = 'heart';
-                } else if (route.name === 'Acerca') {
-                  icon = 'information';
-                } else if (route.name === 'Login') {
-                  icon = 'account-circle';
-                } else if (route.name === 'Registry') {
-                  icon = 'note';
-                }
-                return <Icon name={icon} size={30} color={color} />;
-              },
-              tabBarActiveTintColor: '#18BDB2',
-              tabBarInactiveTintColor: 'gray',
-              headerShown: false,
-            })}
-          >
-            <Tab.Screen name="Login" component={Login} />
-            <Tab.Screen name="Registry" component={Registry} />
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Favoritos" component={Favorites} />
-            <Tab.Screen name="Acerca" component={About} />
-          </Tab.Navigator>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Registry" component={Registry} />
+            <Stack.Screen name="Avatar" component={Avatar} />
+            <Stack.Screen name="Selección" component={PokemonSelection} />
+            <Stack.Screen name="PokedexTab" component={NavigationWithTabs} />
+          </Stack.Navigator>
         </NavigationContainer>
       </ContextProvider>
     </QueryClientProvider>
